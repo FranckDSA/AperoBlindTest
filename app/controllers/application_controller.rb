6e34232491e68729cd127a3664b1897fe2a55fe2
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-25 15:24:33 +0200

    Log in with spotify and redirect to game creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    request.env['omniauth.origin'] || root_path
+    new_game_path
   end
 end
