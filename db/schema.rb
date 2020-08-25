4ef770f306568f293be4025abd6b2d87d8b2d92d
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-25 14:02:42 +0200

    New schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_08_25_110426) do
 
   # These are extensions that must be enabled in order to support this database
