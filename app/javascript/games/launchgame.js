be4ab54d12d744a97dd0d7fbfe11c417591d0230
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-25 18:49:50 +0200

    show player

---
+++ app/javascript/games/launchgame.js
@@ -0,0 +1,10 @@
+const initUpdateGameToPlaying = () => {
+  const launching = document.getElementById("launch-game");
+  launching.addEventListener("click", (event) => {
+    // Do something (callback)
+    launching.dataset.state = "playing";
+
+  });
+}
+
+export { initUpdateGameToPlaying };
