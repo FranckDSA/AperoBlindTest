1918a568a2334a9afa0f267bd0ed4cf165805bc7
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-25 12:26:25 +0200

    First try

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :games
+  devise :omniauthable, :omniauth_providers => [:spotify]
+
+  def self.from_omniauth(auth)
+      where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
