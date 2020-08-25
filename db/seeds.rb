8a5e0c5b6937b1f8ce3d89838cbea145fbfbfe98
Nicolas Kiger <nicolas.kiger@gmail.com>
2020-08-25 23:46:04 +0200

    Belle seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,3 @@
-require 'faker'
-
 p "Destroying tables..."
 
 Playlist.destroy_all
@@ -7,6 +5,10 @@ Game.destroy_all
 # User.destroy_all
 
 p "Tables destroyed!"
+
+sleep(1)
+
+p "Playlists creation..."
 
 playlist_id_cuba = "1KGdyl3UgwzO6wG1Ivksnp"
 playlist_id_diner = "37i9dQZF1DX915ogFalrko"
@@ -32,7 +34,7 @@ playlists.each do |id|
     )
 end
 
-p "Playlists created"
+p "Playlists created!"
 
 # user = User.create!(
 #   email: "nicolas.kiger@gmail.com",
@@ -40,20 +42,49 @@ p "Playlists created"
 #   user_name: "Nico",
 #   uid: "9blz2ine1b7ffzwhkpdjy75za")
 
+sleep(1)
+
+p "Game creation..."
+
 game = Game.create!(
   user_id: User.first.id,
   state: "pending",
   max_score: 20,
   playlist_id: Playlist.first.id,
   game_pin: (('0'..'9').to_a + ('a'..'z').to_a).sample(4).join
-  )
+)
 
-p "Game created"
+p "Game created!"
+
+sleep(1)
+
+p "Tracks creation..."
+#Find playlist game
+game_playlist_spotify_id = Playlist.find(game.playlist_id).spotify_id
+#Playlist game instance
+game_playlist = RSpotify::Playlist.find_by_id(game_playlist_spotify_id)
+#Loop on tracks
+game_playlist.tracks.each do |track|
+  Track.create!(
+    title: track.name,
+    artist: track.artists.first.name,
+    duration_ms: track.duration_ms,
+    spotify_id: track.id,
+    image: track.album.images.first["url"],
+    game_id: game.id
+    )
+end
+p "Tracks created!"
+
+sleep(1)
+
+p "Players creation..."
 
 3.times do
   player = Player.create!(
     user_name: Faker::Superhero.name,
-    game_id: Game.first.id,
+    game_id: game.id,
     score: 0
   )
 end
+p "Players created!"
