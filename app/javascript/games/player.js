772888ae20f122011ef1d05be70fe36f34b647be
Nicolas Kiger <nicolas.kiger@gmail.com>
2020-08-27 18:56:45 +0200

    button buzz ok

--- app/javascript/games/player.js
+++ app/javascript/games/player.js
@@ -20,6 +20,7 @@ const play = ({
 };
 
 const initPlayer = () => {
+  console.log("InitPlayer");
   const playerElement = document.querySelector("#player");
   if (!playerElement) {
     return;
@@ -28,58 +29,58 @@ const initPlayer = () => {
   const currentTrackSpotifyId = playerElement.dataset.currentTrackSpotifyId;
   const gameId = playerElement.dataset.gameId;
   const nextTrackId = playerElement.dataset.nextTrackId;
+  const buzzUser = document.querySelector("#buzz-user");
 
   const csrfToken = document.querySelector('meta[name="csrf-token"]').content;
 
-  const button = document.querySelector('#play');
-  button.addEventListener('click', () => {
-    console.log(button.innerHTML);
-    if (button.innerHTML == "Pause") {
-      button.innerHTML = "Play";
-      button.classList.remove("btn-light");
-      button.classList.add("btn-success");
-      window.spotifyPlayer.pause().then(() => {
-        console.log('Paused!');
-      });
-    } else {
-      button.innerHTML = "Pause";
-      button.classList.remove("btn-success");
-      button.classList.add("btn-light");
-      window.spotifyPlayer.resume().then(() => {
-        console.log('Resumed!');
-      });
-    };
-  });
+  /*NEW CODE*/
 
-  // const button_buzz = document.querySelector('.buzz');
-  // button_buzz.addEventListener('click', () => {
-  //   console.log(button_buzz.innerHTML);
-  //     window.spotifyPlayer.pause().then(() => {
-  //       console.log('Paused!');
-  //     });
-  // });
+  const button = document.querySelector('#play');
+  if (button) {
+    button.addEventListener('click', () => {
+      if (button.innerHTML == "Pause") {
+        button.innerHTML = "Play";
+        button.classList.remove("btn-light");
+        button.classList.add("btn-success");
+        window.spotifyPlayer.pause().then(() => {
+          console.log('Paused!');
+        });
+      } else {
+        button.innerHTML = "Pause";
+        button.classList.remove("btn-success");
+        button.classList.add("btn-light");
+        window.spotifyPlayer.resume().then(() => {
+          console.log('Resumed!');
+        });
+      };
+    });
+  }
 
   const button_nexttrack = document.querySelector('#nexttrack');
-  button_nexttrack.addEventListener('click', () => {
-    fetch(`/games/${gameId}`, {
-      method: "PATCH",
-      body: JSON.stringify({game: {current_track_id: nextTrackId }}),
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'X-CSRF-Token': csrfToken,
-        'Content-Type': 'application/json',
-      },
-      credentials: 'same-origin'
-    })
-    window.spotifyPlayer.nextTrack().then(() => {
-      console.log('Skipped to next track!');
+  if (button_nexttrack) {
+    button_nexttrack.addEventListener('click', () => {
+      fetch(`/games/${gameId}`, {
+        method: "PATCH",
+        body: JSON.stringify({game: {current_track_id: nextTrackId }}),
+        headers: {
+          'X-Requested-With': 'XMLHttpRequest',
+          'X-CSRF-Token': csrfToken,
+          'Content-Type': 'application/json',
+        },
+        credentials: 'same-origin'
+      })
+      window.spotifyPlayer.nextTrack().then(() => {
+        console.log('Skipped to next track!');
+      });
     });
-  });
+  }
 
   const button_endgame = document.querySelector('#end');
-  button_endgame.addEventListener('click', () => {
-    window.spotifyPlayer.disconnect()
-  });
+  if (button_endgame) {
+    button_endgame.addEventListener('click', () => {
+      window.spotifyPlayer.disconnect()
+    });
+  }
 
   if (!document.querySelector("#spotify-js")) {
     const js = document.createElement("script");
@@ -87,14 +88,21 @@ const initPlayer = () => {
     js.src ="https://sdk.scdn.co/spotify-player.js";
     document.head.appendChild(js);
   }
-  console.log(window.spotifyPlayer)
+  console.log("PlayerS", window.spotifyPlayer)
   if (window.spotifyPlayer) {
-    play({
-      playerInstance: window.spotifyPlayer,
-      spotify_uri: `spotify:track:${currentTrackSpotifyId}`,
-    });
+    console.log("BuzzUser",buzzUser);
+    if (buzzUser) {
+       window.spotifyPlayer.pause().then(() => {
+        console.log('Paused!');
+      });
+    } else {
+      play({
+        playerInstance: window.spotifyPlayer,
+        spotify_uri: `spotify:track:${currentTrackSpotifyId}`,
+      });
+    };
     return;
-  }
+  };
 
   window.onSpotifyWebPlaybackSDKReady = () => {
     window.spotifyPlayer = new Spotify.Player({
