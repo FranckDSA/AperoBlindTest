6de8a1d4e58df54ca1703d8024cb7a3c27ff4fc3
Penelope des Dorides <penelopedelaville@gmail.com>
2020-08-28 12:17:36 +0200

    form creation validations

--- app/javascript/games/formcreation.js
+++ app/javascript/games/formcreation.js
@@ -1,9 +1,21 @@
 const initPlaylistChoice = () => {
-  $(document).ready(function(){
-    $(".category-choice").click(function(){
-      $(this).toggleClass("active");
-    });
-  });
-  }
+  // $(document).ready(function(){
+  //   $(".category-choice").click(function(){
+  //     $(this).toggleClass("active");
+  //   });
+  // });
+  const clickPlaylists = document.querySelectorAll(".category-choice")
+  clickPlaylists.forEach((btn) => {
+    btn.addEventListener('click', (event) => {
+      btn.previousElementSibling.click();
+      const clickPlaylistsActive = document.querySelectorAll(".category-choice.active")
+      clickPlaylistsActive.forEach((btnActive) => {
+        btnActive.classList.remove("active")
+      })
+      btn.classList.toggle("active");
+    })
+  })
+}
+
 
 export { initPlaylistChoice };
