cab118d679f11e40783e6c68c9a87ba8c2fc79e1
Nicolas Kiger <nicolas.kiger@gmail.com>
2020-09-01 18:49:05 +0200

    ALLEZ LA PENDING USERRRR VIEW

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,20 +10,20 @@ sleep(1)
 
 p "Playlists creation..."
 
-playlist_id_cuba = "1KGdyl3UgwzO6wG1Ivksnp"
-playlist_id_diner = "37i9dQZF1DX915ogFalrko"
-playlist_id_hits = "37i9dQZF1DXdPec7aLTmlC"
-# playlist_id_annees2000 = "37i9dQZF1DXacPj7eARo6k"
-# playlist_id_bofilm = "37i9dQZF1DX7g9DBqnMitx"
-# playlist_id_dance = "37i9dQZF1DWUwJ0RFwrgQP"
+# playlist_id_cuba = "1KGdyl3UgwzO6wG1Ivksnp"
+# playlist_id_diner = "37i9dQZF1DX915ogFalrko"
+# playlist_id_hits = "37i9dQZF1DXdPec7aLTmlC"
+playlist_id_annees2010 = "37i9dQZF1DX8E06AbSENEw"
+playlist_id_annees2000 = "37i9dQZF1DXacPj7eARo6k"
+playlist_id_annees1990 = "37i9dQZF1DXbTxeAdrVG2l"
 
 playlists = []
-playlists << playlist_id_cuba
-playlists << playlist_id_diner
-playlists << playlist_id_hits
-# playlists << playlist_id_annees2000
-# playlists << playlist_id_bofilm
-# playlists << playlist_id_dance
+# playlists << playlist_id_cuba
+# playlists << playlist_id_diner
+# playlists << playlist_id_hits
+playlists << playlist_id_annees2010
+playlists << playlist_id_annees2000
+playlists << playlist_id_annees1990
 
 playlists.each do |id|
   playlist = RSpotify::Playlist.find_by_id(id)
